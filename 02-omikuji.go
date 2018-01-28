ba6285de0f71fc0b87845b5cd17e273e028d56fe
Morihaya <yukiya.hayashi@gmail.com>
2018-01-28 16:04:06 +0900

    おみくじ完成！

--- 02-omikuji.go
+++ 02-omikuji.go
@@ -1,9 +1,32 @@
 package main
 
-func main() {
-	x := 6
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func main() {
+	// 現在時刻を数値で取得する
+	t := time.Now().UnixNano()
+	// 乱数のたねを設定
+	rand.Seed(t)
+	// xは0-6の間の値になる
+
+	s := rand.Intn(6)
+	println(s)
+	x := s + 1
+	//x := 6
+
+	var msg string
 	if x == 6 {
-		println("大吉")
+		msg = "大吉"
+	} else if x == 5 || x == 4 {
+		msg = "中吉"
+	} else if x == 2 || x == 3 {
+		msg = "小吉"
+	} else if x == 1 {
+		msg = "凶"
 	}
+	fmt.Println(msg)
 }
