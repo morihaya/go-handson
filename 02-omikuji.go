d07f963c570cafa7948536c3659e98535ec38b5a
Morihaya <yukiya.hayashi@gmail.com>
2018-01-28 17:20:36 +0900

    Webアプリが動きました

--- 02-omikuji.go
+++ 02-omikuji.go
@@ -3,57 +3,37 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
 func main() {
-	// 現在時刻を数値で取得する
-	t := time.Now().UnixNano()
-	// 乱数のたねを設定
-	rand.Seed(t)
-	// xは0-6の間の値になる
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	/*
-		for i := 0; i < 7; i = i + 1 {
-			s := rand.Intn(6)
-			//println(s)
-			x := s + 1
-			//x := 6
+func handler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	var message = omikuji(seed)
+	fmt.Fprint(w, message)
+}
 
-			var msg string
-			if x == 6 {
-				msg = "大吉"
-			} else if x == 5 || x == 4 {
-				msg = "中吉"
-			} else if x == 2 || x == 3 {
-				msg = "小吉"
-			} else if x == 1 {
-				msg = "凶"
-			}
-			fmt.Println(i, "日目", msg)
-		}
-	*/
-	i := 0
-	for {
-		s := rand.Intn(6)
-		//println(s)
-		x := s + 1
-		//x := 6
+func omikuji(seed int64) string {
+	rand.Seed(time.Now().UnixNano())
 
-		var msg string
-		if x == 6 {
-			msg = "大吉"
-		} else if x == 5 || x == 4 {
-			msg = "中吉"
-		} else if x == 2 || x == 3 {
-			msg = "小吉"
-		} else if x == 1 {
-			msg = "凶"
-		}
-		fmt.Println(i, "日目", msg)
-		i = i + 1
-		if x == 6 {
-			break
-		}
+	var saikoro int = rand.Intn(6) + 1
+	var unsei string
+
+	switch {
+	case saikoro == 1:
+		unsei = "凶"
+	case 2 <= saikoro && saikoro <= 3:
+		unsei = "吉"
+	case 4 <= saikoro && saikoro <= 5:
+		unsei = "中吉"
+	case saikoro == 6:
+		unsei = "大吉"
 	}
+
+	return unsei
 }
